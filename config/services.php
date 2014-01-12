f27a98944114aade09ac33df6c66c6ca19ccd25f
sasezaki <sasezaki@gmail.com>
2014-01-12 15:27:18 +0900

    default parameter service name should be 'Config'

--- config/services.php
+++ config/services.php
@@ -1,8 +1,8 @@
 <?php
 return (new Zend\ServiceManager\ServiceManager)
-->setService('parameters', include 'parameters.php')
+->setService('Config', include 'parameters.php')
 ->setFactory('view', function($sm) {
     $view = new Phly\Mustache\Mustache;
-    $view->setTemplatePath($sm->get('parameters')['view']['template_path']);
+    $view->setTemplatePath($sm->get('Config')['view']['template_path']);
     return $view;
 });
